77b07f7b28ca78fbca717f60da13d22e0495c66d
tthieu0901 <lonelynightdark@gmail.com>
2023-07-16 21:23:13 +0700

    chores: set up typescript

--- model/block.js
+++ model/block.js
@@ -1,16 +1,18 @@
 class Block {
 
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public timestamp: number;
-    public data: string;
+    index = 0;
+    hash = previousHash = "";
+    timestamp = 0;
+    data = "";
 
-    constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string) {
+    constructor(index, hash, previousHash, timestamp, data) {
         this.index = index;
         this.previousHash = previousHash;
         this.timestamp = timestamp;
         this.data = data;
         this.hash = hash;
     }
+
+    const calculateHash = (index: number, previousHash: string, timestamp: number, data: string): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 }
