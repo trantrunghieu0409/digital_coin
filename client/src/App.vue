914991d0765bc9d207f33a3c9a493c8c6c9db4f5
tthieu0901 <lonelynightdark@gmail.com>
2023-07-23 10:37:06 +0700

    chores: set up client project

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,85 +1,86 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!--
+=========================================================
+* Vue Argon Dashboard 2 - v3.0.0
+=========================================================
 
+* Product Page: https://creative-tim.com/product/vue-argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div
+    v-show="this.$store.state.layout === 'landing'"
+    class="landing-bg h-100 bg-gradient-primary position-fixed w-100"
+  ></div>
+  <sidenav
+    :custom_class="this.$store.state.mcolor"
+    :class="[
+      this.$store.state.isTransparent,
+      this.$store.state.isRTL ? 'fixed-end' : 'fixed-start'
+    ]"
+    v-if="this.$store.state.showSidenav"
+  />
+  <main
+    class="main-content position-relative max-height-vh-100 h-100 border-radius-lg"
+  >
+    <!-- nav -->
+    <navbar
+      :class="[navClasses]"
+      :textWhite="
+        this.$store.state.isAbsolute ? 'text-white opacity-8' : 'text-white'
+      "
+      :minNav="navbarMinimize"
+      v-if="this.$store.state.showNavbar"
+    />
+    <router-view />
+    <app-footer v-show="this.$store.state.showFooter" />
+    <configurator
+      :toggle="toggleConfigurator"
+      :class="[
+        this.$store.state.showConfig ? 'show' : '',
+        this.$store.state.hideConfigButton ? 'd-none' : ''
+      ]"
+    />
+  </main>
 </template>
+<script>
+import Sidenav from "./main_components/Sidenav";
+import Configurator from "@/main_components/Configurator.vue";
+import Navbar from "@/main_components/Navbars/Navbar.vue";
+import AppFooter from "@/main_components/Footer.vue";
+import { mapMutations } from "vuex";
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  name: "App",
+  components: {
+    Sidenav,
+    Configurator,
+    Navbar,
+    AppFooter
+  },
+  methods: {
+    ...mapMutations(["toggleConfigurator", "navbarMinimize"])
+  },
+  computed: {
+    navClasses() {
+      return {
+        "position-sticky bg-white left-auto top-2 z-index-sticky":
+          this.$store.state.isNavFixed && !this.$store.state.darkMode,
+        "position-sticky bg-default left-auto top-2 z-index-sticky":
+          this.$store.state.isNavFixed && this.$store.state.darkMode,
+        "position-absolute px-4 mx-0 w-100 z-index-2": this.$store.state
+          .isAbsolute,
+        "px-0 mx-4": !this.$store.state.isAbsolute
+      };
+    }
+  },
+  beforeMount() {
+    this.$store.state.isTransparent = "bg-transparent";
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+};
+</script>
